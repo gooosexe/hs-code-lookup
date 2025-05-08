f95b8624d3f86c1dc9f5fdb7362a0ed3369c7848
oliver huang <45010534+gooosexe@users.noreply.github.com>
2025-05-08 01:51:08 -0400

    Delete src/app/chapters directory

--- src/app/chapters/scrape.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-for i in range(1, 100):
-    chapter = f"{i:02d}"  # two-digit format
-    url = f"https://www.cbsa-asfc.gc.ca/trade-commerce/tariff-tarif/2025/html/00/ch{chapter}-eng.html"
-    subprocess.run(["wget", url])
